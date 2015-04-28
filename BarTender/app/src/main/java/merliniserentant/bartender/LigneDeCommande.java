85ed8c1526956fcfc8d6452c6f54ba2176afd1ac
rvercauteren <roselien_vercauteren@hotmail.com>
2015-04-28 17:14:48 +0200

    Ajout de bouton Commander et Annuler
    Le bouton Commander ajoute une nouvelle Ligne de Commande à la BDD (mais reste à paufiner)
    Le bouton Annuler retour au "menu"

--- BarTender/app/src/main/java/merliniserentant/bartender/LigneDeCommande.java
+++ BarTender/app/src/main/java/merliniserentant/bartender/LigneDeCommande.java
@@ -6,20 +6,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class LigneCommande extends ActionBarActivity implements View.OnClickListener {
+public class LigneCommande extends ActionBarActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
+    private Button commander;
+    private Button annuler;
     private Spinner boisson;
     private EditText quantité;
     private EditText tabl;
-    private int num;
+    private int num = 1;
     private String login;
     private String bsn;
     private int qté;
@@ -78,36 +83,59 @@ public class LigneCommande extends ActionBarActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ligne_commande);
-        // Locate the button
-        boisson = (Spinner) findViewById(R.id.boisson);
-        List<String> listBoisson = ?; // compléter avec boisson de la BDD
-        ArrayAdapter list = new ArrayAdapter(this, R.layout.activity_ligne_commande, listBoisson);
+
+        // localise le spinner
+       boisson = (Spinner) findViewById(R.id.boisson);
+       boisson.setOnItemClickListener(this);
+        // créer la liste
+        List<String> listBoisson = new ArrayList<String>();
+        // compléter avec boisson de la BDD
+        listBoisson.add("Orval"); // faire une boucle pour remplir ?
+        // créer un adaptateur
+        ArrayAdapter list = new ArrayAdapter(this, android.R.layout.simple_spinner_item, listBoisson);
+        // layout
+        list.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        // relie l'adaptateur au spinner
         boisson.setAdapter(list);
-        // Locate the EditText
+
+        // localise les EditText
         quantité = (EditText) findViewById(R.id.quantité);
         tabl = (EditText) findViewById(R.id.table);
-        // Get values from EditText
+        // Reprend les valeurs des EditText
         qté = Integer.parseInt(quantité.getText().toString());
         table = Integer.parseInt(tabl.getText().toString());
-        // Attribuer num et récupérer login ??
+        // récupérer login ??
+
+        // localise les Button
+        commander = (Button) findViewById(R.id.commander);
+        annuler = (Button) findViewById(R.id.annuler);
+        commander.setOnClickListener(this);
+        annuler.setOnClickListener(this);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ligne_commande, menu);
-        return true;
-    }
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.boisson:
-                Intent intent = new Intent(LigneCommande.this, ListeBoisson.class);
+            case R.id.commander:
+                // insérer nouvelle ligne de commande dans la BDD
+                LigneCommande newLigne = new LigneCommande(num, login, bsn, qté, table);
+                // insertLigneCommande (newLigne);
+                num = num + 1;
+            case R.id.annuler:
+                // retourner à la page précédente
+                Intent intent = new Intent(LigneCommande.this, MenuFr.class); // MenuFr = main
                 startActivity(intent);
         }
     }
 
+    @Override
+    public void onItemClick (AdapterView parent, View view, int position, long id){
+        // récupérer l'item sélectionner
+        String item = parent.getItemAtPosition(position).toString();
+        // montrer l'item sélectionner
+        Toast.makeText(parent.getContext(), item, Toast.LENGTH_LONG).show();
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
