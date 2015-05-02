87a09887692813fce6060069f02aaadfd6705f35
jgheysen <julie.gheysen@yahoo.fr>
2015-05-02 11:08:34 +0200

    début du completage de consult_details.java

--- DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/consult_details.java
+++ DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/consult_details.java
@@ -1,39 +1,47 @@
 package merliniserentant.bar_tender;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
 
 
 public class consult_details extends Activity {
+
+    private Boisson currentBoisson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consult_details);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_consult_details, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        int number = getIntent().getIntExtra("", -1);
+        if (number==-1) {
+            throw new RuntimeException("Aucun numéro de boisson n'est spécifié");
         }
+        currentBoisson = BoissonDAO.getBoissonwithNumboisson(number);
 
-        return super.onOptionsItemSelected(item);
+        TextView name = (TextView) findViewById(R.id.consult_details_name);
+        name.setText(currentBoisson.getNom());
+
+        TextView description = (TextView) findViewById(R.id.consult_details_description);
+        description.setText(currentBoisson.getDescription());
+
+        Bitmap bitmap = currentBoisson.getLogotype();
+
+        if(bitmap!=null) {
+            ImageView picture = (ImageView)findViewById(R.id.consult_details_picture);
+            picture.setImageBitmap(bitmap);
+        }
+        else {
+            View pictureLL = findViewById(R.id.consult_details_picture_ll);
+            pictureLL.setVisibility(View GONE);
+        }
     }
+
+
 }
