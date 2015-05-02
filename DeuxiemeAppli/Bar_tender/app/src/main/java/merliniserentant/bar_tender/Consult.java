ecec3e95fa551df4b7bf454a3018739ab203a776
jgheysen <julie.gheysen@yahoo.fr>
2015-05-02 12:29:11 +0200

    modification onItemClick de Consult.java

--- DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/Consult.java
+++ DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/Consult.java
@@ -26,6 +26,8 @@ public class Consult extends Activity{
     private ArrayList<Boisson> listboisson;
     private List<String> listnom;
     private ListView listviewboisson;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -45,6 +47,9 @@ public class Consult extends Activity{
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listnom);
         listviewboisson.setAdapter(adapter);
+        // Indique que le clic d'un élément de la liste doit appeler la méthode onItemClick d
+        // cette classe (this).
+        listviewboisson.setOnItemClickListener(this);
 
         boissondao.close();
         retour.setOnClickListener(new View.OnClickListener() {
@@ -56,12 +61,12 @@ public class Consult extends Activity{
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    public void onItemClick(AdapterView<?> parent, View view, int position) {
         Intent intent = new Intent(this, consult_details.class);
 
         // Pour la ligne en dessous j'ai un beug pour savoir comment passer le numero de la boisson
         // à la prochaine activity :/ ce qu'il y a mis après numboisson ca vient du tp 10
-        intent.putExtra("numBoisson", collectedItems.get(position).getId());
+        intent.putExtra("numBoisson", listboisson.get(position).getNumboisson());
         startActivity(intent);
     }
 
