107dd8c72acf61195bd73aa9ee2d57094ddfbbee
rvercauteren <roselien_vercauteren@hotmail.com>
2015-05-03 15:44:43 +0200

    Addition.java, fonctionne pas encore
    erreur l.32 avec parseInt

---
+++ DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/Addition.java
@@ -0,0 +1,54 @@
+package merliniserentant.bar_tender;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class Addition extends Activity {
+    private Button mPasserelle;
+    private EditText table;
+    public static double prix;
+    String numtbl;
+    public static int numTable;
+    AdditionDAO adao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addition);
+
+        adao = new AdditionDAO(this);
+
+        table = (EditText) findViewById(R.id.tableAddition);
+        numtbl = table.getText().toString();
+        numTable = Integer.parseInt(numtbl);
+
+        mPasserelle = (Button) findViewById(R.id.premier);
+        mPasserelle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adao.open();
+                if (adao.getAdditionToPay(numTable) == null){
+                    Toast.makeText(Addition.this, "Aucune addition pour cette table", Toast.LENGTH_SHORT).show();
+                }
+                prix = adao.getTotalPrix(numTable);
+                Intent secondeActivite = new Intent(Addition.this, Paiement.class);
+                startActivity(secondeActivite);
+                adao.close();
+            }
+        });
+    }
+
+
+
+
+
+}
