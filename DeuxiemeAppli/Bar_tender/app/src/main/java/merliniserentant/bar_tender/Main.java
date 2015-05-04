bfb88fc3542925ccffce7bef30bd664dfc5b9796
miserentant <merlin.iserentant@student.uclouvain.be>
2015-05-04 18:08:13 +0200

    Petite modif

--- DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/Main.java
+++ DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/Main.java
@@ -17,18 +17,26 @@ public class Main extends Activity {
     private Button addition = null;
     private Button commande = null;
     private Button inventaire = null;
+    private BoissonDAO boissondao = null;
 
 
     @Override
     public void onCreate(Bundle savedInstancedState) {
         super.onCreate(savedInstancedState);
-
+        boissondao = new BoissonDAO(this);
         setContentView(R.layout.main);
         retour = (Button) findViewById(R.id.retour);
         addition = (Button) findViewById(R.id.addition);
         commande = (Button) findViewById(R.id.commande);
         inventaire = (Button) findViewById(R.id.inventaire);
         carte = (Button) findViewById(R.id.carte);
+        if(boissondao.getLangue().equals("Néerlandais")){
+            retour.setText("Terug");
+            addition.setText("Betalen");
+            commande.setText("Bestellen");
+            inventaire.setText("Stock");
+            carte.setText("Kaart");
+        }
 
 
         String typeoflogin = Utilisateur.connectedUser.gettype();
@@ -48,8 +56,8 @@ public class Main extends Activity {
         carte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 Intent intent = new Intent(Main.this, carte.class);
-                 startActivity(intent);
+                Intent intent = new Intent(Main.this, carte.class);
+                startActivity(intent);
             }
         });
         commande.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +71,8 @@ public class Main extends Activity {
         addition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 Intent intent = new Intent(Main.this, Addition.class);
-                 startActivity(intent);
+                Intent intent = new Intent(Main.this, Addition.class);
+                startActivity(intent);
             }
         });
         inventaire.setOnClickListener(new View.OnClickListener() {
