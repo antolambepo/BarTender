473c415c7c8d3c96851e19904c0e59c12ad1d653
AliceNs <alice.nsengimana@student.uclouvain.be>
2015-05-07 00:07:01 +0200

    Creation des classes RetraitBoisson et AjoutBoisson

---
+++ DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/AjoutBoisson.java
@@ -0,0 +1,12 @@
+package merliniserentant.bar_tender;
+
+import android.app.Activity;
+
+/**
+ * Created by Alice on 07-05-15.
+ *
+ * Classe qui permet au propriétaire d'ajouter une boisson à l'inventaire
+ */
+public class AjoutBoisson extends Activity{
+
+}
---
+++ DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/RetraitBoisson.java
@@ -0,0 +1,11 @@
+package merliniserentant.bar_tender;
+
+import android.app.Activity;
+
+/**
+ * Created by Alice on 07-05-15.
+ *
+ * Classe qui permet au propriétaire de retirer une boisson de l'inventaire
+ */
+public class RetraitBoisson extends Activity{
+}
