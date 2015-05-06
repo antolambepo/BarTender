b69b1aa1a166074417e919adf14d5696392c501b
Roselien Vercauteren <Roselien@MBP-de-admin.lan>
2015-05-06 22:58:12 +0200

    modif

--- DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/MyListViewAdapter.java
+++ DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/MyListViewAdapter.java
@@ -76,7 +76,7 @@ public class MyListViewAdapter extends BaseAdapter {
 
         nameTextView.setText(boisson.getNom());
         stockTextView.setText(Integer.toString(boisson.getStock()));
-        stockMaxTextView.setText(Integer.toString(boisson.getlStockmax()));
+        stockMaxTextView.setText(Integer.toString(boisson.getStockmax()));
         seuilTextView.setText(Integer.toString(boisson.getSeuil()));
 
         return convertView;
