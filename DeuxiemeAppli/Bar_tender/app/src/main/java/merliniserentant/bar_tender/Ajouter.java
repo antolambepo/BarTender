fd13dfd6ebd98d800f0c1869f532df684c05bd18
miserentant <merlin.iserentant@student.uclouvain.be>
2015-05-07 17:31:38 +0200

    je suis ue merde

--- DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/Ajouter.java
+++ DeuxiemeAppli/Bar_tender/app/src/main/java/merliniserentant/bar_tender/Ajouter.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
